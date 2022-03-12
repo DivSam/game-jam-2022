f8fb5b7ecf1546b28ef48c7f34d18a15b10bb2b7
DivSam <48162524+DivSam@users.noreply.github.com>
2022-03-12 03:41:38 -0500

    cawliss man

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    private const float PLAYER_DISTANCE_FROM_SPAWN = 100f;
+
+    [SerializeField] private Transform startPoint;
+
+    [SerializeField] private Transform subsequentPoint;
+
+    [SerializeField] private Transform player;
+
+    private Vector3 lastEndPosition;
+    private void Awake()
+    {
+        lastEndPosition = startPoint.Find("Endpoint").position;
+
+        int parts = 5;
+
+        for (int i = 0; i<parts; i++)
+        {
+            SpawnLevelPart();
+        }
+    }
+
+    private void Update()
+    {
+        if(Vector3.Distance(player.position,lastEndPosition) < PLAYER_DISTANCE_FROM_SPAWN)
+        {
+            SpawnLevelPart();
+        }
+    }
+
+
+    private void SpawnLevelPart()
+    {
+        Transform lastPartTransform = SpawnNewPart(lastEndPosition);
+
+        lastEndPosition = lastPartTransform.Find("Endpoint").position;
+    }
+    private Transform SpawnNewPart(Vector3 spawnPosition)
+    {
+        Transform levelPartTransform = Instantiate(subsequentPoint, spawnPosition, Quaternion.identity);
+
+        return levelPartTransform;
+
+    }
+}
